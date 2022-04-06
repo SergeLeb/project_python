b47911181e52f1dc5dac0c01348c05c47c1fe071
serg <sml85@bk.ru>
2022-04-06 21:05:34 +0300

    task_1

--- 4_1.py
+++ 4_1.py
@@ -1,1 +1,13 @@
-test
+from sys import argv
+
+def salary():
+    try:
+        time, rate, bonus = map(float, argv[1:])
+        if time > 160:
+            print(f"salary - {(time * rate) + (bonus * (time - 160))}") # если выработка (выше нормы), то премия
+        else:
+            print(f"(salary - {time * rate}") # если выработка стандарт, то без премии
+    except ValueError:
+        print("введите данные: выработку часов в месяц, часовую ставку в у/е, премию в у/е за час переработки.")
+
+salary()
